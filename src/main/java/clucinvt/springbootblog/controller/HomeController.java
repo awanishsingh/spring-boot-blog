8d33f2b7fa9976332b5104f36842d4ea145b30de
Cam Luc <cluc.invt@gmail.com>
2018-04-02 20:25:47 -0500

    add css, jqueryte, login page

--- src/main/java/clucinvt/springbootblog/controller/HomeController.java
+++ src/main/java/clucinvt/springbootblog/controller/HomeController.java
@@ -6,12 +6,18 @@
 package clucinvt.springbootblog.controller;
 
 import clucinvt.springbootblog.repository.BlogPostRepository;
+import java.security.Principal;
+import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -19,15 +25,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HomeController {
-    
+
     @Autowired
     private BlogPostRepository blogPostRepo;
-    
-    @RequestMapping("/")
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model, Pageable pageable) {
         model.addAttribute("posts", blogPostRepo.findPublishedPosts(pageable).getContent());
         Pageable titlePageable = PageRequest.of(0, 10);
         model.addAttribute("titles", blogPostRepo.findPublishedPostTitles(titlePageable).getContent());
         return "index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Principal principal) {
+        if (principal != null) {
+            Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+            if (authorities.stream().anyMatch(a -> "admin".equals(a.getAuthority()))) {
+                return "redirect:/admin";
+            }
+        }
+        return "login";
     }
 }
