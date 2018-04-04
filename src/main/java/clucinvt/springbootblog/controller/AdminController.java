ee9cb295e68ad0ccb61706fa04d51e81cc87b0a8
Cam Luc <cluc.invt@gmail.com>
2018-04-03 20:30:36 -0500

    add write, publish, unpublish

--- src/main/java/clucinvt/springbootblog/controller/AdminController.java
+++ src/main/java/clucinvt/springbootblog/controller/AdminController.java
@@ -5,11 +5,21 @@
  */
 package clucinvt.springbootblog.controller;
 
+import clucinvt.springbootblog.domain.BlogPost;
+import clucinvt.springbootblog.domain.User;
 import clucinvt.springbootblog.repository.BlogPostRepository;
+import clucinvt.springbootblog.repository.UserRepository;
+import java.security.Principal;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *
@@ -21,10 +31,58 @@ public class AdminController {
 
     @Autowired
     private BlogPostRepository blogPostRepo;
+    @Autowired
+    private UserRepository userRepo;
 
     @RequestMapping(value = {"", "/"})
     public String index(Model model) {
         model.addAttribute("posts", blogPostRepo.findAllPostDescriptors());
         return "admin/index";
+    }
+
+    @RequestMapping(value = "/write", method = {RequestMethod.GET})
+    public String write(Model model) {
+        BlogPost newPost = new BlogPost();
+        model.addAttribute("newpost", newPost);
+        return "admin/write";
+    }
+
+    @RequestMapping(value = "/write/{id}", method = {RequestMethod.GET})
+    public String write(@PathVariable("id") int id, Model model) {
+        model.addAttribute("post", blogPostRepo.findById(id).get());
+        return "admin/write";
+    }
+
+    @RequestMapping(value = "/write", method = {RequestMethod.POST})
+    public String write(@ModelAttribute BlogPost post, BindingResult errors, Model model, Principal principal) {
+        User author = userRepo.findByUsername(principal.getName());
+        post.setAuthor(author);
+        if (post.getId() == 0) { // it is a new post
+            post.setCreateDate(new Date());
+            blogPostRepo.save(post);
+            return "redirect:/admin/";
+        } else { // it is an existing post
+            BlogPost existingPost = blogPostRepo.findById(post.getId()).get();
+            existingPost.setTitle(post.getTitle());
+            existingPost.setBody(post.getBody());
+            blogPostRepo.save(existingPost);
+            return "redirect:/admin/";
+        }
+    }
+    
+    @RequestMapping(value = "publish", method = {RequestMethod.POST})
+    public String publish(@RequestParam("id") int id) {
+        BlogPost post = blogPostRepo.findById(id).get();
+        post.setPublishDate(new Date());
+        blogPostRepo.save(post);
+        return "redirect:/admin/";
+    }
+
+    @RequestMapping(value = "unpublish", method = {RequestMethod.POST})
+    public String unpublish(@RequestParam("id") int id) {
+        BlogPost post = blogPostRepo.findById(id).get();
+        post.setPublishDate(null);
+        blogPostRepo.save(post);
+        return "redirect:/admin/";
     }
 }
