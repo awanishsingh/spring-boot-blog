49a1d48e406b628fcb6b41c550617e303a7cbdfd
Cam Luc <cluc.invt@gmail.com>
2018-04-18 20:53:21 -0500

    add ability to comment on a post

--- src/main/java/clucinvt/springbootblog/controller/PostController.java
+++ src/main/java/clucinvt/springbootblog/controller/PostController.java
@@ -6,37 +6,76 @@
 package clucinvt.springbootblog.controller;
 
 import clucinvt.springbootblog.controller.exception.NotFoundException;
+import clucinvt.springbootblog.domain.BlogComment;
 import clucinvt.springbootblog.domain.BlogPost;
+import clucinvt.springbootblog.repository.BlogCommentRepository;
 import clucinvt.springbootblog.repository.BlogPostRepository;
+import java.util.Date;
 import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
  * @author Cam Luc
  */
-@Controller
+@Controller()
+@RequestMapping("/post")
 public class PostController {
 
     @Autowired
     private BlogPostRepository blogPostRepo;
+    @Autowired
+    private BlogCommentRepository blogCommentRepo;
 
-    @RequestMapping(value = "/post/{id}", method = {RequestMethod.GET})
-    public String detail(@PathVariable("id") int id, Model model) throws NotFoundException {
+    @RequestMapping(value = "/{id}", method = {RequestMethod.GET})
+    public String detail(@PathVariable("id") int id, Model model, BlogComment blogComment) throws NotFoundException {
+
+        loadDetailData(id, model);
+        model.addAttribute("blogComment", blogComment);
+
+        return "post";
+    }
+
+    @RequestMapping(value = "/{id}/comment", method = {RequestMethod.POST})
+    public String comment(@PathVariable("id") int id, @Valid BlogComment blogComment,
+            BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) throws NotFoundException {
+
+        if (bindingResult.hasErrors()) {
+            loadDetailData(id, model);
+            return "post";
+        }
+
+        Optional<BlogPost> post = blogPostRepo.findById(id);
+        if (!post.isPresent()) {
+            throw new NotFoundException();
+        }
+        blogComment.setBlogPost(post.get());
+        blogComment.setCreateDate(new Date());
+        blogCommentRepo.save(blogComment);
+
+        redirectAttributes.addFlashAttribute("message", "Your comment was submitted");
+
+        return "redirect:/post/" + id;
+    }
+
+    private void loadDetailData(int id, Model model) throws NotFoundException {
         Optional<BlogPost> post = blogPostRepo.findById(id);
         if (!post.isPresent()) {
             throw new NotFoundException();
         }
         model.addAttribute("post", post.get());
+
         Pageable titlePageable = PageRequest.of(0, 10);
         model.addAttribute("titles", blogPostRepo.findPublishedPostTitles(titlePageable).getContent());
-        return "post";
     }
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 -- Be cautious when you edit this file. It initializes data for unit tests and integration tests.
+-- Default admin's password is 123
 
 INSERT INTO user (name, email, username, password) VALUES ('Administrator', 'admin@localhost', 'admin', '$2a$04$VXb9pY5bt3YcBd4i4h4/ZeuRC0vF4Evm18m4OKraib9CdvfEbwzZm')
 
