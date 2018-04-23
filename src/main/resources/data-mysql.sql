b346d721b2e9b2d5414903544c7604ec43c1b5a6
Cam Luc <cluc.invt@gmail.com>
2018-04-23 11:11:06 -0500

    add default user for production environment

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,1 +1,3 @@
+-- Default admin's password is 123
 
+INSERT INTO user (name, email, username, password) VALUES ('Administrator', 'admin@localhost', 'admin', '$2a$04$VXb9pY5bt3YcBd4i4h4/ZeuRC0vF4Evm18m4OKraib9CdvfEbwzZm')
