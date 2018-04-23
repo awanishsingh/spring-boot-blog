0f35c1ea05540b15117a9f440352ce51d65378d9
Cam Luc <cluc.invt@gmail.com>
2018-04-23 16:01:45 -0500

    rename repository tests to follow naming convention

--- src/test/java/clucinvt/springbootblog/repository/BlogPostRepositoryIT.java
+++ src/test/java/clucinvt/springbootblog/repository/BlogPostRepositoryIT.java
@@ -24,7 +24,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BlogPostRepositoryTest {
+public class BlogPostRepositoryIT {
 
     @Autowired
     private BlogPostRepository blogPostRepo;
